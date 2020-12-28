16988a91ee406e537a8604f2d804612d3c555d7d
Sunny Dasgupta <SunnyDasgupta11@gmail.com>
2020-12-28 17:48:38 +0000

    Multi-tenant system set uip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
+  set_current_tenant_by_subdomain_or_domain(:team, :subdomain, :domain)
+  
   before_action :authenticate_user!
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_28_135912) do
+ActiveRecord::Schema.define(version: 2020_12_28_160954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2020_12_28_135912) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "team_id", null: false
+    t.index ["team_id"], name: "index_posts_on_team_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "subdomain"
+    t.string "domain"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_12_28_135912) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "teams"
 end
